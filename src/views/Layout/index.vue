8f688bde07f8c217d6d63a8dbf8e223b80cfb55f
EKlingD <2080116915@qq.com>
2024-06-20 09:35:47 +0800

    pinia优化导航数据

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,13 @@ import LayoutNav from "@/views/Layout/componenents/LayoutNav.vue";
 import LayoutHeader from "@/views/Layout/componenents/LayoutHeader.vue";
 import LayoutFooter from "@/views/Layout/componenents/LayoutFooter.vue";
 import LayoutFixed from "@/views/Layout/componenents/LayoutFixed.vue";
+
+//触发获取导航列表的action
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
+const CategoryStore = useCategoryStore()
+
+onMounted(() => CategoryStore.getCategory())
 </script>
 
 <template>
