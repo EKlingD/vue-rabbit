42c587ce2f4fe86fa779d894f9f1032f899f1193
EKlingD <2080116915@qq.com>
2024-06-20 08:18:57 +0800

    引入图标字体

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,13 +1,12 @@
 <script setup>
-
-import LayoutNav from '@/views/Layout/componenents/LayoutNav.vue'
-import LayoutHeader from '@/views/Layout/componenents/LayoutHeader.vue'
-import LayoutFooter from '@/views/Layout/componenents/LayoutFooter.vue'
+import LayoutNav from "@/views/Layout/componenents/LayoutNav.vue";
+import LayoutHeader from "@/views/Layout/componenents/LayoutHeader.vue";
+import LayoutFooter from "@/views/Layout/componenents/LayoutFooter.vue";
 </script>
 
 <template>
   <LayoutNav />
   <LayoutHeader />
-  <RouterView />
   <LayoutFooter />
+  <RouterView />
 </template>
