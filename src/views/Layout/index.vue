055e0e40bf42c30aeecd732cd19d318654ec0750
EKlingD <2080116915@qq.com>
2024-06-20 09:10:54 +0800

    layout-吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutNav from "@/views/Layout/componenents/LayoutNav.vue";
 import LayoutHeader from "@/views/Layout/componenents/LayoutHeader.vue";
 import LayoutFooter from "@/views/Layout/componenents/LayoutFooter.vue";
+import LayoutFixed from "@/views/Layout/componenents/LayoutFixed.vue";
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <LayoutFooter />
