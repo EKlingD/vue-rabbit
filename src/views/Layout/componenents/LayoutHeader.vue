35a6bb7f987583c0a9684dcbb9d77861b3d7f785
EKlingD <2080116915@qq.com>
2024-06-21 08:21:44 +0800

    一级分类-路由配置

--- src/views/Layout/componenents/LayoutHeader.vue
+++ src/views/Layout/componenents/LayoutHeader.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useCategoryStore } from "@/stores/category";
 
-const categoryStore = useCategoryStore()
+const categoryStore = useCategoryStore();
 </script>
 
 <template>
@@ -10,9 +10,17 @@ const categoryStore = useCategoryStore()
       <h1 class="logo">
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
+
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+        <li class="home">
+          <RouterLink to="/">首页</RouterLink>
+        </li>
+        <li
+          class="home"
+          v-for="item in categoryStore.categoryList"
+          :key="item.id"
+        >
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
