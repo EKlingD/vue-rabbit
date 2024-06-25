84ba8f18064871c83c343946b78e3c942b65db05
EKlingD <2080116915@qq.com>
2024-06-25 14:26:05 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,40 +1,50 @@
 <script setup>
-
-
 //表单校验（账号名+密码）
 import { ref } from "vue";
 
 //1.准备表单对象
-const form= ref({
-  account:'',
-  password:'',
-  agree:true
-})
+const form = ref({
+  account: "",
+  password: "",
+  agree: true,
+});
 
 //2.准备规则对象
 const rules = {
-  account: [
-    {required: true,message:'用户名不能为空',trigger:'blur'}
-  ],
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
   password: [
-    {required: true,message:'密码不能为空',trigger:'blur'},
-    {min:6,max:14,message:'密码长度为6-14个字符',trigger:'blur'}
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码长度为6-14个字符", trigger: "blur" },
   ],
-  agree:[
+  agree: [
     {
-      validator: (rule,value,callback)=>{
+      validator: (rule, value, callback) => {
         console.log(value);
         //自定义校检逻辑
         //勾选就通过 不勾选就不通过
-        if(value){
-          callback()
-        }else{
-          callback(new Error('请勾选协议'))
+        if (value) {
+          callback();
+        } else {
+          callback(new Error("请勾选协议"));
         }
-      }
+      },
+    },
+  ],
+};
+
+//3.获取form实例做统一校验
+const formRef = ref(null);
+const doLogin = () => {
+  //调用实例方法
+  formRef.value.validate((valid) => {
+    //valid：所有表单都通过校验  才为true
+    console.log(valid);
+    //以valid做为判断条件 如果通过校验才执行登录逻辑
+    if (valid) {
+      //TODO LOGIN
     }
-  ]
-}
+  });
+};
 </script>
 
 <template>
@@ -59,6 +69,7 @@ const rules = {
         <div class="account-box">
           <div class="form">
             <el-form
+              ref="formRef"
               :model="form"
               :rules="rules"
               label-position="right"
@@ -76,7 +87,9 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin"
+                >点击登录</el-button
+              >
             </el-form>
           </div>
         </div>
