74db3116d117ead0318b6eeddbd51c1c63e4ac9f
EKlingD <2080116915@qq.com>
2024-06-26 09:25:20 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,17 @@ export const useUserStore = defineStore('user', () => {
    const getUserInfo = async ({ account, password }) => {
       const res = await loginAPI({ account, password })
       userInfo.value = res.result
-   }    //3.以对象的格式把state和action return
+   }
+   //退出时清除用户信息
+   const clearUserInfo = () => {
+      userInfo.value = {}
+   }
+
+   //3.以对象的格式把state和action return
    return {
       userInfo,
-      getUserInfo
+      getUserInfo,
+      clearUserInfo
    }
 }, {
    persist: true,
