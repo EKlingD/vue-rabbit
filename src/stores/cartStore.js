52a6b3dfb61e196cacc39f5efc46d7b3b075c25a
EKlingD <2080116915@qq.com>
2024-06-27 13:51:18 +0800

    登录时合并购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -111,7 +111,8 @@ export const useCartStore = defineStore('cart', () => {
         addCart,
         delCart,
         singleCheck,
-        allCheck
+        allCheck,
+        updateNewList
     }
 }, {
     persist: true,
